cde52871078eccdedddd906e78befbb9457afee8
wasixd <164501586+wasixd@users.noreply.github.com>
2024-03-24 11:19:34 +0500

    WASI-MD

--- config.js
+++ config.js
@@ -93,7 +93,7 @@ module.exports = {
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "WASI"  ).toUpperCase(),
 
 
 
