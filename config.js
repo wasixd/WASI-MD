6e2d9d20ccdd2e239ed88edc0893b98784c6fb05
wasixd <164501586+wasixd@users.noreply.github.com>
2024-03-25 11:38:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="jastinmtewa@yahoo.com"
 global.location="Morogoro,Tanzania."
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://wasitech10:WASImd##12@cluster0.eclhxiz.mongodb.net/?retryWrites=true&w=majority"
 
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "120363023983262391@g.us"
