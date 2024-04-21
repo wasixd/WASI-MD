4bc69baac45b4fcef144fc686d04cc40f010394e
the  develpor <164501586+wasixd@users.noreply.github.com>
2024-04-21 13:23:06 +0500

    WASI MD

---
+++ plugins/_menu.js
@@ -0,0 +1,84 @@
+const zaky = aidin;
+(function (amiylah, ajak) {
+  const chris = aidin, dorinna = amiylah();
+  while (true) {
+    try {
+      const axa = -parseInt(chris(310)) / 1 + -parseInt(chris(258)) / 2 * (parseInt(chris(282)) / 3) + parseInt(chris(272)) / 4 + -parseInt(chris(286)) / 5 * (-parseInt(chris(308)) / 6) + -parseInt(chris(270)) / 7 * (-parseInt(chris(260)) / 8) + -parseInt(chris(256)) / 9 * (-parseInt(chris(314)) / 10) + -parseInt(chris(263)) / 11;
+      if (axa === ajak) break; else dorinna.push(dorinna.shift());
+    } catch (eulalia) {
+      dorinna.push(dorinna.shift());
+    }
+  }
+}(keshone, 651565));
+const os = require("os"), Config = require("../config");
+let {fancytext, tiny, runtime, formatp, prefix} = require("../lib");
+const long = String[zaky(253)](8206), readmore = long[zaky(268)](4001), giftech_patch = require(zaky(267)), trend_usage = (() => {
+  const asar = ((peteris, ailynn) => {
+    const sonza = aidin, azrah = () => Math[sonza(307)](), iiesha = habeeb => Math.floor(habeeb), jovaniel = (thedis, ashiana) => thedis * ashiana, markk = (mahadev, jefrin) => mahadev + jefrin, deor = (azyria, baisley) => azyria - baisley, kenyara = jovaniel(azrah(), deor(ailynn, peteris + 1)), diezel = markk(iiesha(kenyara), peteris);
+    return diezel;
+  })(1, 99);
+  return asar;
+})(), database_info = (() => {
+  const kaysaan = ((keya, romik) => {
+    const adalisa = aidin, donte = () => Math[adalisa(307)](), keja = lavarious => Math[adalisa(304)](lavarious), kanesha = (dominicka, jaeliyah) => dominicka * jaeliyah, ranndy = (sanyah, khalisa) => sanyah + khalisa, naryia = (yeimy, moayad) => yeimy - moayad, kyelle = kanesha(donte(), naryia(romik, keya + 1)), maripat = ranndy(keja(kyelle), keya);
+    return maripat;
+  })(1, 499);
+  return kaysaan;
+})();
+function aidin(darniesha, nickolous) {
+  const flavis = keshone();
+  return aidin = function (kassandra, mural) {
+    kassandra = kassandra - 253;
+    let missty = flavis[kassandra];
+    return missty;
+  }, aidin(darniesha, nickolous);
+}
+giftech_patch.smd({cmdname: zaky(317), desc: "Help list", react: "🪀", desc: zaky(287), type: "user", filename: __filename}, async (younique, arriyanah) => {
+  const lexilynn = zaky;
+  try {
+    const {commands: _0x17bebb} = require(lexilynn(306));
+    if (arriyanah[lexilynn(280)](" ")[0]) {
+      let bralynn = [];
+      const tywanna = _0x17bebb.find(aunya => aunya[lexilynn(295)] === arriyanah.split(" ")[0].toLowerCase());
+      tywanna && (bralynn[lexilynn(266)]("*🔉Command:* " + tywanna[lexilynn(295)]), tywanna[lexilynn(296)] && bralynn[lexilynn(266)](lexilynn(262) + tywanna[lexilynn(296)]), tywanna[lexilynn(259)] && tywanna[lexilynn(259)][0] && bralynn.push("*💁Alias:* " + tywanna[lexilynn(259)][lexilynn(303)](", ")), tywanna[lexilynn(309)] && bralynn.push(lexilynn(319) + tywanna[lexilynn(309)]), tywanna.use && bralynn.push(lexilynn(288) + prefix + tywanna[lexilynn(295)] + " " + tywanna.use + lexilynn(313)), tywanna[lexilynn(279)] && bralynn[lexilynn(266)](lexilynn(288) + tywanna[lexilynn(279)] + lexilynn(313)), await younique[lexilynn(290)](bralynn[lexilynn(303)]("\n")));
+    }
+    let alexandor, carlee, simao, tekelia, amariyanna, heathe, thaddis;
+    Config[lexilynn(317)] === "" && (alexandor = Math[lexilynn(304)](Math[lexilynn(307)]() * 4) + 1);
+    if (alexandor === 1 || Config[lexilynn(317)][lexilynn(281)]()[lexilynn(257)]("1") || Config.menu.toLowerCase()[lexilynn(312)]("menu1")) carlee = lexilynn(315) + Config[lexilynn(318)] + lexilynn(274), heathe = lexilynn(300), simao = lexilynn(292), tekelia = "┌『", amariyanna = "』", heathe = " | ", thaddis = lexilynn(276); else alexandor === 2 || Config.menu.trim().startsWith("2") || Config[lexilynn(317)].toLowerCase().includes(lexilynn(311)) ? (carlee = lexilynn(273) + Config[lexilynn(318)] + lexilynn(298), heathe = lexilynn(305), simao = "╰════════════···▸", tekelia = "┌〈", amariyanna = "〉", heathe = "¶│▸ ", thaddis = lexilynn(278)) : (carlee = lexilynn(269) + Config.botname + lexilynn(264), heathe = lexilynn(289), simao = "╰═══════════════⊷", tekelia = lexilynn(283), amariyanna = "❏", heathe = "│", thaddis = lexilynn(265));
+    const leslye = {};
+    _0x17bebb[lexilynn(316)](async (vearl, chrisina) => {
+      const derric = lexilynn;
+      vearl[derric(284)] === false && vearl[derric(295)] !== undefined && (!leslye[vearl.category] && (leslye[vearl[derric(296)]] = []), leslye[vearl[derric(296)]].push(vearl[derric(295)]));
+    });
+    const rhoan = younique[lexilynn(254)], bernarr = younique.date;
+    let kella = lexilynn(277) + carlee + lexilynn(277) + heathe + lexilynn(261) + Config.ownername + "\n  " + heathe + lexilynn(301) + runtime(process[lexilynn(302)]()) + "\n  " + heathe + lexilynn(294) + formatp(os.totalmem() - os[lexilynn(293)]()) + lexilynn(277) + heathe + lexilynn(291) + rhoan + lexilynn(277) + heathe + " *ᴅᴀᴛᴇ ᴛᴏᴅᴀʏ:* " + bernarr + lexilynn(277) + heathe + lexilynn(297) + _0x17bebb.length + lexilynn(277) + simao + lexilynn(271) + readmore + "\n";
+    for (const sheraton in leslye) {
+      kella += tekelia + " *" + tiny(sheraton) + "* " + amariyanna + "\n";
+      if (arriyanah.toLowerCase() === sheraton[lexilynn(285)]()) {
+        kella = tekelia + " *" + tiny(sheraton) + "* " + amariyanna + "\n";
+        for (const krishana of leslye[sheraton]) {
+          kella += heathe + " " + fancytext(krishana, 1) + "\n";
+        }
+        kella += thaddis + "\n";
+        break;
+      } else {
+        for (const aiza of leslye[sheraton]) {
+          kella += heathe + " " + fancytext(aiza, 1) + "\n";
+        }
+        kella += thaddis + "\n";
+      }
+    }
+    kella += Config.caption;
+    const jaiquan = {caption: kella, ephemeralExpiration: 3e3};
+    return await younique.sendUi(younique[lexilynn(275)], jaiquan, younique);
+  } catch (asmita) {
+    await younique[lexilynn(255)](asmita + lexilynn(299), asmita);
+  }
+});
+function keshone() {
+  const shavia = ["* ]", "\nCommand: menu", "┃ ✗", " *ᴜᴘᴛɪᴍᴇ:* ", "uptime", "join", "floor", "¶│▸", "../lib", "random", "36fwxjjG", "desc", "847344UydqHA", "menu2", "includes", "```", "60eLffXy", "┏﹝ *", "map", "menu", "botname", "*💁Description:* ", "fromCharCode", "time", "error", "907704jpyAvd", "startsWith", "1246502nSLgxa", "alias", "2832hWwAgW", " *ᴏᴡɴᴇʀ:* ", "*💁Category:* ", "2351239hJpPwH", "  〙", "╰════════════─⊷", "push", "../lib/plugins", "repeat", "╭〘  ", "10192mReTyR", "\n  \n  ______________________________\n *©²⁰²⁴ 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐔𝐒𝐄𝐑-𝐁𝐎𝐓 𝐁𝐘-𝐖𝐀𝐒𝐈*\n  ______________________________\n  \n", "3388612jHTvrf", "┌═[ *", "* ﹞", "chat", "\n└═════════════〤", "\n  ", "\n│╰══════════···▸▸", "usage", "split", "trim", "3QNsMvz", "╭─❏", "dontAddCommandList", "toLowerCase", "306830jBpxFr", "To show all avaiable commands.", "*〽️Usage:*\n ```", "│ │", "reply", " *ʟɪᴠᴇ ᴛɪᴍᴇ:* ", "┗═════════════〤", "freemem", " *ʀᴀᴍ:* ", "pattern", "category", " *ᴄᴏᴍᴍᴀɴᴅs:* "];
+  keshone = function () {
+    return shavia;
+  };
+  return keshone();
+}
